7b63139b75bdd4f100c8cf24a6f48fec9e2f7f67
Farhan Arbi Utomo <192727269+Chieellesaxions@users.noreply.github.com>
2025-10-23 17:27:26 +0700

    Update hello_world.c

--- src/hello_world.c
+++ src/hello_world.c
@@ -8,3 +8,8 @@ int main() {
     printf("Hello World!\n");
     return 0;
 }
+
+// Filename: hello_world.c
+// Description: Basic example of Display "Hello World!" on the screen in C
+// Author: Farhan Arbi Utomo
+// Date: October 23, 2025
