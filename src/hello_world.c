893ae8117abcf5b869cea60132e805cfd77452c0
Farhan Arbi Utomo <192727269+Chieellesaxions@users.noreply.github.com>
2025-10-23 19:35:58 +0700

    Update hello_world.c

--- src/hello_world.c
+++ src/hello_world.c
@@ -2,14 +2,15 @@
 // Description: Display "Hello World!" on the screen
 // Created by: Farhan Arbi Utomo
 
-#include <stdio.h>
+#include <stdio.h> // Header file untuk fungsi input dan output
 
 int main() {
-    printf("Hello World!\n");
-    return 0;
+    printf("Hello World!\n"); // Menampilkan teks ke layar (tanpa newline)
+    return 0; // Mengembalikan nilai 0 (menandakan program selesai dengan sukses)
 }
 
 // Filename: hello_world.c
 // Description: Basic example of Display "Hello World!" on the screen in C
 // Author: Farhan Arbi Utomo
 // Date: October 23, 2025
+
