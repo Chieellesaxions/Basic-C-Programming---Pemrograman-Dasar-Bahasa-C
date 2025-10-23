1aa2565c3ef3a3e17a6e7d032a5ab165b5fd698c
Farhan Arbi Utomo <192727269+Chieellesaxions@users.noreply.github.com>
2025-10-23 17:21:14 +0700

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+// Program: hello_world.c
+// Description: Display "Hello World!" on the screen
+// Created by: Farhan Arbi Utomo
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
